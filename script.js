0cc946994587b4293fb0ec1c7954ec38f4e59381
brecec <brendaramirez@brendas-mbp.lan>
2023-05-23 17:09:49 -0500

    updatinghtml

--- script.js
+++ script.js
@@ -13,21 +13,22 @@ function handleSearchForm(event) {
         console.error('You need a search input value!');
         return;
     }
-    getLocation();
+    getLocation(searchInputVal);
 }
 
-function getLocation() {
+function getLocation(cityInput) {
+    console.log("this is in geolocation:", cityInput);
     // location
     var newName = document.getElementById("cityInput");
     var cityName = document.getElementById("cityName");
     cityName.innerHTML = "--" + newName.value + "--"
 
-    searchApi();
+    searchApi(cityInput);
 }
 
 
 function searchApi(city) {
-    
+
     var latLonUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
     fetch(latLonUrl).then(function (response) {
         return response.json();
@@ -40,22 +41,23 @@ function searchApi(city) {
         }).then(function (data) {
             console.log(data);
         })
-        // var currentWeather = document.querySelector("#currentweatherdetails");
-        // var tempEl = document.querySelector(".temp");
-        // var humidityEl= document.querySelector(".humidity");
-        // var windSpeed = document.querySelector(".windspeed");
-        // tempEl.innerHTML = Number(data[0].list.main.temp);
-        // humidityEl.innerHTML = Number(data[0].list.main.humidity);
-        // windSpeed.innerHTML = Number (data[0].list.wind.speed);
+
 
     })
 }
 
-// function printForecast() {
-
-    
-
-// }
+function printForecast(data) {
+    // var currentWeather = document.querySelector("#currentweatherdetails");
+    var tempEl = document.querySelector(".temp");
+    var humidityEl = document.querySelector(".humidity");
+    var windSpeed = document.querySelector(".windspeed");
+    var imageIcon = document.getElementById("#img")
+    tempEl.innerHTML = Number(data.list[0].main.temp);
+    humidityEl.innerHTML = Number(data.list[0].main.humidity);
+    windSpeed.innerHTML = Number(data.list[0].wind.speed);
+    //src = "https://openweathermap.org/img/wn/" + data.list[0].weather.icon + ".png";
+    // for loop here for 5 day forecast
+}
 
 
 
