e53e425bc46c37581109d67313fc4e94a9667558
brecec <brendaramirez@brendas-mbp.lan>
2023-05-22 22:41:17 -0500

    searchapifunction

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var searchBoxEl = document.querySelector(".search-box");
 var resultTextEl = document.querySelector(".result-text");
-var resultContentEl = document.querySelector("#currentweatherdetails");
-var forecastEl = document.getElementById ("#forecast");
+
+var forecastEl = document.getElementById("#forecast");
+var apiKey = "6d31b19eb7d07b9d70d1e6b48620ae60";
 
 function handleSearchForm(event) {
     event.preventDefault();
@@ -12,24 +13,51 @@ function handleSearchForm(event) {
         console.error('You need a search input value!');
         return;
     }
+    getLocation();
 }
 
-function getLocation(){
+function getLocation() {
     // location
-    var searchLocParam = document
-    
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--" + newName.value + "--"
 
-  //  searchApi();
+    searchApi();
 }
 
-// function searchApi (query,format){
-    // var locQueryapi=""
-// }
 
-// function printForecast(resultObj){
+function searchApi(city) {
+    
+    var latLonUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+    fetch(latLonUrl).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        var locQueryUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        fetch(locQueryUrl).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+        })
+        // var currentWeather = document.querySelector("#currentweatherdetails");
+        // var tempEl = document.querySelector(".temp");
+        // var humidityEl= document.querySelector(".humidity");
+        // var windSpeed = document.querySelector(".windspeed");
+        // tempEl.innerHTML = Number(data[0].list.main.temp);
+        // humidityEl.innerHTML = Number(data[0].list.main.humidity);
+        // windSpeed.innerHTML = Number (data[0].list.wind.speed);
+
+    })
+}
+
+// function printForecast() {
+
+    
 
 // }
 
 
 
 searchBoxEl.addEventListener("submit", handleSearchForm);
+// searchApi();
