f68def8a5ec91de6586153135bcb8a0035a15bbe
brecec <brendaramirez@brendas-mbp.lan>
2023-05-24 20:56:46 -0500

    printcurrentweather

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var searchBoxEl = document.querySelector(".search-box");
 var resultTextEl = document.querySelector(".result-text");
+// var currentDayEl = $("#currentDay")
+// var currentweather = document.querySelector(".currentweather");
 
 var forecastEl = document.getElementById("#forecast");
 var apiKey = "6d31b19eb7d07b9d70d1e6b48620ae60";
+
 
 function handleSearchForm(event) {
     event.preventDefault();
@@ -13,6 +16,7 @@ function handleSearchForm(event) {
         console.error('You need a search input value!');
         return;
     }
+
     getLocation(searchInputVal);
 }
 
@@ -35,31 +39,52 @@ function searchApi(city) {
     }).then(function (data) {
         var lat = data[0].lat;
         var lon = data[0].lon;
-        var locQueryUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        var locQueryUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
         fetch(locQueryUrl).then(function (response) {
             return response.json();
         }).then(function (data) {
-            console.log(data);
+            // console.log(data);
+            var temp = data.list[0].main.temp;
+            var humidity = data.list[0].main.humidity;
+            var windspeed = data.list[0].wind.speed;
+            var weathericon = data.list[0].weather[0].icon;
+            var today = data.list[0].dt_txt;
+            var iconurl = "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
+            var todaysDate = document.querySelector(".todaysdate");
+            var tempEl = document.querySelector(".temp");
+            var humidityEl = document.querySelector(".humidity");
+            var windSpeed = document.querySelector(".windspeed");
+            var weatherIcon = document.getElementById("img1");
+            weatherIcon.setAttribute("src", iconurl);
+            todaysDate.innerHTML = "Date: " + today;
+            tempEl.innerHTML = "Temperature: " + temp + "°F";
+            humidityEl.innerHTML = "Humidity: " + humidity;
+            windSpeed.innerHTML = "Wind Speed: " + windspeed;
+            console.log(temp, humidity, windspeed)
+            printForecast(data);
         })
-
 
     })
 }
 
-function printForecast(data) {
+function printForecast(forecastData) {
     // var currentWeather = document.querySelector("#currentweatherdetails");
-    var tempEl = document.querySelector(".temp");
-    var humidityEl = document.querySelector(".humidity");
-    var windSpeed = document.querySelector(".windspeed");
-    var imageIcon = document.getElementById("#img")
-    tempEl.innerHTML = Number(data.list[0].main.temp);
-    humidityEl.innerHTML = Number(data.list[0].main.humidity);
-    windSpeed.innerHTML = Number(data.list[0].wind.speed);
-    //src = "https://openweathermap.org/img/wn/" + data.list[0].weather.icon + ".png";
-    // for loop here for 5 day forecast
+    console.log(forecastData);
+    //for loop here for 5 day forecast
+    for (i = 8; i < forecastData.list.length; i=i+8) {
+        // console.log(forecastData.list[0].main.temp);
+        // console.log(forecastData.list[i].dt_txt);
+        var forecastContainer = document.getElementById("forecast");
+        var date = document.createElement("h3");
+
+        forecastContainer.textContent = forecastData.list[i].dt_txt;
+
+    }
 }
 
 
 
+
 searchBoxEl.addEventListener("submit", handleSearchForm);
-// searchApi();
+
+
